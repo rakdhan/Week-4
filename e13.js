16d700a2ce3799ce41f9fdf0f597765d3e71d1f3
rakdhan <58630970+rakdhan@users.noreply.github.com>
2020-01-18 16:25:51 +0700

    today

---
+++ e13.js
@@ -0,0 +1,59 @@
+function deepSum (arr) {
+    // Code disini
+    var result = 0
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            for (let k = 0; k < arr[i][j].length; k++) {
+                result += arr[i][j][k]
+            }            
+        }
+    }
+    // console.log(result)
+    if (result === 0 ) {
+        return 'No number'
+    }
+
+    return result
+  }
+  
+  //TEST CASE
+  console.log(deepSum([
+    [
+      [4, 5, 6],
+      [9, 1, 2, 10],
+      [9, 4, 3]
+    ],
+    [
+      [4, 14, 31],
+      [9, 10, 18, 12, 20],
+      [1, 4, 90]
+    ],
+    [
+      [2, 5, 10],
+      [3, 4, 5],
+      [2, 4, 5, 10]
+    ]
+  ])); // 316
+  
+  console.log(deepSum([
+    [
+      [20, 10],
+      [15],
+      [1, 1]
+    ],
+    [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      [2],
+      [9, 11]
+    ],
+    [
+      [3, 5, 1],
+      [1, 5, 3],
+      [1]
+    ],
+    [
+      [2]
+    ]
+  ])); // 156
+  
+  console.log(deepSum([])); // No number
