fafd7d513c2b38ea315a6210a47af08826ce275f
rakdhan <58630970+rakdhan@users.noreply.github.com>
2020-03-11 17:53:21 +0700

    mantap kak

--- 11shoppingTime.js
+++ 11shoppingTime.js
@@ -13,7 +13,7 @@ function shoppingTime(memberId, money) {
     else if (memberId === undefined && money === undefined) {
         return 'Mohon maaf, toko X hanya berlaku untuk member saja'
     }
-    else if (money < 50000) {
+    else if (money < 50000 || money === undefined || money === '') {
         return 'Mohon maaf, uang tidak cukup'
     }
     var obj = {
@@ -50,5 +50,5 @@ console.log(shoppingTime('82Ku8Ma742', 170000));
 //  [ 'Casing Handphone' ],
 // changeMoney: 120000 }
 console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
-console.log(shoppingTime('234JdhweRxa53', 15000)); //Mohon maaf, uang tidak cukup
-console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
+console.log(shoppingTime('234JdhweRxa53', '')); //Mohon maaf, uang tidak cukup
+console.log(shoppingTime()); //Mohon maaf, toko X hanya berlaku untuk member saja
